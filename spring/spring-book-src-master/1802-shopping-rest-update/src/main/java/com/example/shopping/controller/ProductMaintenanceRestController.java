c8f9e77680032cac3103ce802c93fc0dfceb2cc7
rohgibong <rohgibong@gmail.com>
2024-08-23 20:35:24 +0900

    그림으로 배우는 스프링6입문

--- spring/spring-book-src-master/1802-shopping-rest-update/src/main/java/com/example/shopping/controller/ProductMaintenanceRestController.java
+++ spring/spring-book-src-master/1802-shopping-rest-update/src/main/java/com/example/shopping/controller/ProductMaintenanceRestController.java
@@ -1,14 +1,19 @@
 package com.example.shopping.controller;
 
+import java.net.URI;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.shopping.exception.DataNotFoundException;
+import com.example.shopping.input.ProductMaintenanceInput;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.shopping.entity.Product;
 import com.example.shopping.service.ProductMaintenanceService;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/products")
@@ -26,6 +31,37 @@ public class ProductMaintenanceRestController {
 
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable String id) {
-        return productMaintenanceService.findById(id);
+        Product product = productMaintenanceService.findById(id);
+        if(product == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return product;
     }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProduct(@PathVariable String id, @RequestBody ProductMaintenanceInput productMaintenanceInput) {
+        productMaintenanceInput.setId(id);
+        productMaintenanceService.update(productMaintenanceInput);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Void> registerProduct(@Validated @RequestBody ProductMaintenanceInput productMaintenanceInput) {
+        Product product = productMaintenanceService.register(productMaintenanceInput);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(product.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable String id) {
+        productMaintenanceService.delete(id);
+    }
+
 }
